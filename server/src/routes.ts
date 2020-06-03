93ad3c03c8bbc6b2a1087717b76f6d6eb00b0ab4
williamoliverpro <williamoliverpro@gmail.com>
2020-06-03 14:20:33 -0300

    secondcommit.2
    secondcommit.2

---
+++ server/src/routes.ts
@@ -0,0 +1,55 @@
+import express from 'express'
+import knex from './database/connection'
+
+const routes = express.Router()
+
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*')
+
+    const serializedItems = items.map(item => {
+        return { 
+            id: item.id,
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+         }
+    })
+
+    return response.json(serializedItems)
+})
+
+routes.post('/points', async (request, response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items,
+    } = request.body
+
+    const ids = await knex('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    })
+
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id: ids[0],
+        }
+    })
+
+    await knex('point_items').insert(pointItems)
+
+    return response.json({ sucess: true })
+})
+
+export default routes
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,12 @@
 import express from 'express'
+import path from 'path'
+import routes from './routes'
 
 const app = express()
 
-app.get('/users', (request, response) => {
-    console.log('Listagem de usuário')
+app.use(express.json())
+app.use(routes)
 
-    response.json([
-        'William',
-        'William',
-    ])
-})
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333)
